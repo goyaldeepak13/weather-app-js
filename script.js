c4aa46a049a693d9b0d7fc5c22a41c81a831a33d
goyaldeepak13 <deepakgoyalbari@gmail.com>
2023-09-02 00:47:23 +0530

    Update code

--- script.js
+++ script.js
@@ -11,25 +11,42 @@ const getWeatehr = (city) => {
 
     fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city= ' + city, options)
         .then(response => response.json())
+ 
         .then(response => {
-
-            // console.log(response)
-            temp.innerHTML = response.temp
-            temp2.innerHTML = response.temp
-            feels_like.innerHTML = response.feels_like
-            humidity.innerHTML = response.humidity
-            humidity2.innerHTML = response.humidity
-            min_temp.innerHTML = response.min_temp
-            max_temp.innerHTML = response.max_temp
-            wind_speed2.innerHTML = response.wind_speed
-            wind_speed.innerHTML = response.wind_speed
-            wind_degrees.innerHTML = response.wind_degrees
-            sunrise.innerHTML = response.sunrise
-            sunset.innerHTML = response.sunset
+            temp.innerHTML = response.temp;
+            temp2.innerHTML = response.temp;
+            feels_like.innerHTML = response.feels_like;
+            humidity.innerHTML = response.humidity;
+            humidity2.innerHTML = response.humidity;
+            min_temp.innerHTML = response.min_temp;
+            max_temp.innerHTML = response.max_temp;
+            wind_speed2.innerHTML = response.wind_speed;
+            wind_speed.innerHTML = response.wind_speed;
+            wind_degrees.innerHTML = response.wind_degrees;
+        
+            // Convert and display sunrise and sunset timestamps
+            sunrise.innerHTML = unixTimestampToDateTime(response.sunrise);
+            sunset.innerHTML = unixTimestampToDateTime(response.sunset);
         })
+
+
+
         .catch(err => console.error(err));
 }
 submit.addEventListener("click", (e) => {
     e.preventDefault(); // page load nahi
     getWeatehr(city.value)
 })
+
+ 
+
+
+// Function to convert Unix timestamp to a readable date and time format
+function unixTimestampToDateTime(unixTimestamp) {
+    const date = new Date(unixTimestamp * 1000); // Convert to milliseconds
+    const hours = date.getHours();
+    const minutes = "0" + date.getMinutes();
+    const seconds = "0" + date.getSeconds();
+    const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return formattedTime;
+}
